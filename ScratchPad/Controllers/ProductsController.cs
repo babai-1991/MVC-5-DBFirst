d27f7a37518dda60b7c7a0616ef46762310d0935
Suvankar Das <iambabai0@gmail.com>
2021-01-16 22:14:52 +0530

    Insert Product

--- ScratchPad/Controllers/ProductsController.cs
+++ ScratchPad/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,11 +17,32 @@ namespace ScratchPad.Controllers
             return db;
         }
         // GET: Category
-        public ActionResult Index()
+        public ActionResult Index(string searchQuery="")
+        {
+            ViewBag.SearchTerm = searchQuery;
+            var db = DatabaseOperation();
+            List<Product> products = db.Products.Where(p => p.ProductName.Contains(searchQuery)).ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(long id)
         {
             var db = DatabaseOperation();
-            List<Product> products = db.Products.ToList();
-            return View(products);
+            Product product = db.Products.SingleOrDefault(p => p.ProductID == id);
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            db.Products.Add(product);
+            int result = db.SaveChanges();
+            return RedirectToAction("Index", "Products");
         }
     }
 }
