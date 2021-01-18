ad1ffdb16f64b3660695166f426a0497adfbbddf
Suvankar Das <iambabai0@gmail.com>
2021-01-18 12:45:52 +0530

    Insert Update Delete Category and Brand and displaying category brand in product index page using navigation property

--- ScratchPad/Controllers/BrandController.cs
+++ ScratchPad/Controllers/BrandController.cs
@@ -15,11 +15,63 @@ namespace ScratchPad.Controllers
             return db;
         }
         // GET: Brand
-        public ActionResult Index()
+        public ActionResult Index(string searchBrand = "")
         {
+            ViewBag.SearchTerm = searchBrand;
             var db = DatabaseOperation();
-            List<Brand> brands = db.Brands.ToList();
+            List<Brand> brands = db.Brands.Where(b => b.BrandName.Contains(searchBrand)).ToList();
             return View(brands);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Brand brand)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            db.Brands.Add(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Brand");
+        }
+
+        public ActionResult Details(long id)
+        {
+            Brand brand = DatabaseOperation().Brands.SingleOrDefault(b => b.BrandID == id);
+            return View(brand);
+        }
+
+        public ActionResult Delete(long id)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Brand brand = db.Brands.SingleOrDefault(br => br.BrandID == id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Brand");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Brand brand = db.Brands.SingleOrDefault(b => b.BrandID == id);
+            return View(brand);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Brand brand)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Brand oldBrand = db.Brands.SingleOrDefault(br => br.BrandID == brand.BrandID);
+            //Now update
+            if (oldBrand != null)
+            {
+                oldBrand.BrandName = brand.BrandName;
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Brand");
         }
     }
 }
--- ScratchPad/Controllers/CategoryController.cs
+++ ScratchPad/Controllers/CategoryController.cs
@@ -15,11 +15,63 @@ namespace ScratchPad.Controllers
             return db;
         }
         // GET: Category
-        public ActionResult Index()
+        public ActionResult Index(string searchCategory = "")
         {
+            ViewBag.SearchTerm = searchCategory;
             var db = DatabaseOperation();
-            List<Category> categories = db.Categories.ToList();
+            List<Category> categories = db.Categories.Where(cat => cat.CategoryName.Contains(searchCategory)).ToList();
             return View(categories);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category cat)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            db.Categories.Add(cat);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
+        public ActionResult Details(long id)
+        {
+            Category category = DatabaseOperation().Categories.SingleOrDefault(cat => cat.CategoryID == id);
+            return View(category);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Category cat = db.Categories.SingleOrDefault(c => c.CategoryID == id);
+            return View(cat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category cat)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Category oldCat = db.Categories.SingleOrDefault(ctr => ctr.CategoryID == cat.CategoryID);
+            //Now update
+            if (oldCat != null)
+            {
+                oldCat.CategoryName = cat.CategoryName;
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            var db = new EFDBFirstDatabaseEntities();
+            Category cat = db.Categories.SingleOrDefault(ctr => ctr.CategoryID == id);
+            db.Categories.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category");
         }
     }
 }
