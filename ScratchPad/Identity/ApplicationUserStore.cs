64da00d29845e9d5b2d0fc4511f6ffe74d90f157
Suvankar Das <iambabai0@gmail.com>
2021-01-26 12:43:01 +0530

    Implement ApplicationUser,ApplicationDbContext,ApplicationUserStore,ApplicationUserManager

---
+++ ScratchPad/Identity/ApplicationUserStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ScratchPad.Identity
+{
+    /*contains methods for manipulating the users information from the database.
+     It directly interact with ApplicationDbContext internally.*/
+    public class ApplicationUserStore:UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext dbContext):base(dbContext)
+        {
+            
+        }
+    }
+}
---
+++ ScratchPad/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(ScratchPad.Startup))]
+
+namespace ScratchPad
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                //This means we are going to store the user's identity details inside a cookie temporarily when he login.
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                //If not logged in , redirects to Account/Login URL
+                LoginPath = new PathString("~/Account/Login")
+            });
+        }
+    }
+}
