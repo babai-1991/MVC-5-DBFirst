560debe25a1b62721ff4360a9de5fd87572ebdf9
Suvankar Das <iambabai0@gmail.com>
2021-04-13 04:59:21 +0530

    Application_Error global filter

--- ScratchPad/Global.asax.cs
+++ ScratchPad/Global.asax.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -9,7 +12,22 @@ namespace ScratchPad
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+
+        private void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+            //code for storing exception details.
+            string message =
+                String.Format(
+                    $"Occured on {DateTime.Now.ToString("F")} Message: {exception.Message} , " +
+                    $"Type: {exception.GetType().ToString()}, Source: {exception.Source}");
+            StreamWriter writer = File.AppendText(HttpContext.Current.Request.PhysicalApplicationPath + "\\Errorlog.txt");
+            writer.WriteLine(message);
+            writer.Close();
+
+            Response.Redirect("~/error.html");
         }
     }
 }
